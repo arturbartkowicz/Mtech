3299268613b1b20769b8f048db11e2944281ae05
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-10-14 16:19:20 +0200

    added basic navigation styles

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -8,8 +9,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="nav flex flex-col items-center justify-center w-full flex-1 px-20 text-center"></main>
-
+      <Nav />
+      <main className="flex flex-col items-center w-full flex-1 px-20 text-center">
+        <p>1 section</p>
+        <p>2 section</p>
+      </main>
       <footer className="flex items-center justify-center w-full h-12 border-t bg-black">
         <p className="text-white">
           <span className="text-red-800">M</span>TECH PRECISION &#169; 2021
