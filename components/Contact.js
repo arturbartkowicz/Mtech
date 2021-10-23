858f2c9e5d07aac2eaf4770aa9c26138333f95af
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-10-23 18:50:05 +0200

    added contact boxes

--- components/Contact.js
+++ components/Contact.js
@@ -1,7 +1,21 @@
 export default function Contact() {
   return (
     <div id="contact" className="contact">
-      <h2>Contact Section</h2>
+      <div className="contact__box contact__box--1">
+        <h3 className="contact__header">Pytania i odpowiedzi:</h3>
+        <div>Najczęstsze pytanie #1</div>
+        <div>Najczęstsze pytanie #2</div>
+        <div>Najczęstsze pytanie #3</div>
+        <div>Najczęstsze pytanie #4</div>
+        <div>Najczęstsze pytanie #5</div>
+        <div>Button</div>
+      </div>
+      <div className="contact__box contact__box--2">
+        <h3 className="contact__header">Kontakt:</h3>
+      </div>
+      <div className="contact__box contact__box--3">
+        <h3 className="contact__header">Nasi partnerzy:</h3>
+      </div>
     </div>
   );
 }
