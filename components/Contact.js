0d40aa9254614902b831d3a85695d910e9d9eb31
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-10-24 12:44:23 +0200

    added okntakt informations

--- components/Contact.js
+++ components/Contact.js
@@ -12,6 +12,23 @@ export default function Contact() {
       </div>
       <div className="contact__box contact__box--2">
         <h3 className="contact__header">Kontakt:</h3>
+        <p className="contact__text">MTech Precision</p>
+        <p className="contact__text">
+          Grochowe 113A
+          <br />
+          39-332 tuszów Narodowy
+        </p>
+        <p className="contact__text">
+          Tel. <u>+48 576 280 272</u>
+        </p>
+        <p className="contact__text">
+          NIP: 8171945042
+          <br />
+          REGON: 381465643
+        </p>
+        <p className="contact__text">
+          E-mail: <u>mtechoffice113a@gmail.com</u>
+        </p>
       </div>
       <div className="contact__box contact__box--3">
         <h3 className="contact__header">Nasi partnerzy:</h3>
