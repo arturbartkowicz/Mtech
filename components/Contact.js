37f582729eaf8d49499a075bb220c0e2ef2203fd
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-10-24 13:19:24 +0200

    added btn to a footer

--- components/Contact.js
+++ components/Contact.js
@@ -3,12 +3,26 @@ export default function Contact() {
     <div id="contact" className="contact">
       <div className="contact__box contact__box--1">
         <h3 className="contact__header">Pytania i odpowiedzi:</h3>
-        <div className="contact__questionContainer">Najczęstsze pytanie #1</div>
-        <div className="contact__questionContainer">Najczęstsze pytanie #2</div>
-        <div className="contact__questionContainer">Najczęstsze pytanie #3</div>
-        <div className="contact__questionContainer">Najczęstsze pytanie #4</div>
-        <div className="contact__questionContainer">Najczęstsze pytanie #5</div>
-        <div className="contact__questionContainer">Button</div>
+        <div className="w-full">
+          <div className="contact__questionContainer">
+            Najczęstsze pytanie #1
+          </div>
+          <div className="contact__questionContainer">
+            Najczęstsze pytanie #2
+          </div>
+          <div className="contact__questionContainer">
+            Najczęstsze pytanie #3
+          </div>
+          <div className="contact__questionContainer">
+            Najczęstsze pytanie #4
+          </div>
+          <div className="contact__questionContainer">
+            Najczęstsze pytanie #5
+          </div>
+        </div>
+        <div className="w-full flex justify-center mt-auto mb-4">
+          <button className="btn">pokaz więcej</button>
+        </div>
       </div>
       <div className="contact__box contact__box--2">
         <h3 className="contact__header">Kontakt:</h3>
