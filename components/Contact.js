eb7438dc3a7d5ca0465e7a584829f659c5b42e51
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-10-26 18:59:50 +0200

    added slick slide to a project

--- components/Contact.js
+++ components/Contact.js
@@ -1,4 +1,18 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export default function Contact() {
+  const settings = {
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div id="contact" className="contact">
       <div className="contact__box contact__box--1">
@@ -46,6 +60,28 @@ export default function Contact() {
       </div>
       <div className="contact__box contact__box--3">
         <h3 className="contact__header">Nasi partnerzy:</h3>
+        <div className="text-white w-40 h-40">
+          <Slider {...settings}>
+            <div className="w-full flex justify-center">
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+          </Slider>
+        </div>
       </div>
     </div>
   );
