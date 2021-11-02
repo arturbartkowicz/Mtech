3121adf3a857fd92eb993f3446a35f32387ae5a0
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-11-02 19:45:27 +0100

    added slider to a nasze realizacje

--- components/Cooperation.js
+++ components/Cooperation.js
@@ -2,35 +2,25 @@ export default function Cooperation() {
   return (
     <div id="cooperation" className="cooperation">
       <div className="cooperation__container">
-        <h2 className="heading">Cooperation Section</h2>
+        <h2 className="heading">Warunki współpracy</h2>
         <p className="paragraphText">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-          exercitationem hic. Quam quo blanditiis distinctio magni ipsa ab,
-          cumque aspernatur doloremque impedit mollitia nam fugiat quaerat
-          deserunt culpa consequuntur facere. Lorem ipsum, dolor sit amet
-          consectetur adipisicing elit. Voluptas, itaque saepe. Perspiciatis
-          dolores maiores quibusdam ea magni? Nisi soluta accusamus, quia omnis,
-          vitae esse ab, est reprehenderit debitis vel veniam. Lorem, ipsum
-          dolor sit amet consectetur adipisicing elit. Itaque similique expedita
-          ipsa. Laudantium vero similique voluptate unde, ratione, praesentium
-          possimus non doloremque adipisci dolorum quaerat ab reiciendis animi?
-          Odit, harum? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis expedita quod, ullam, similique dolorem non animi eaque ea
-          nulla nisi a cum neque rem alias eveniet nam! Nihil, qui
-          necessitatibus! Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Soluta, exercitationem hic. Quam quo blanditiis distinctio magni
-          ipsa ab, cumque aspernatur doloremque impedit mollitia nam fugiat
-          quaerat deserunt culpa consequuntur facere. Lorem ipsum, dolor sit
-          amet consectetur adipisicing elit. Voluptas, itaque saepe.
-          Perspiciatis dolores maiores quibusdam ea magni? Nisi soluta
-          accusamus, quia omnis, vitae esse ab, est reprehenderit debitis vel
-          veniam. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Itaque similique expedita ipsa. Laudantium vero similique voluptate
-          unde, ratione, praesentium possimus non doloremque adipisci dolorum
-          quaerat ab reiciendis animi? Odit, harum? Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Reiciendis expedita quod, ullam,
-          similique dolorem non animi eaque ea nulla nisi a cum neque rem alias
-          eveniet nam! Nihil, qui necessitatibus!
+          Opis warunków współpracy. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, exercitationem hic.
+          Quam quo blanditiis distinctio magni ipsa ab, cumque aspernatur doloremque impedit mollitia nam fugiat quaerat
+          deserunt culpa consequuntur facere. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, itaque
+          saepe. Perspiciatis dolores maiores quibusdam ea magni? Nisi soluta accusamus, quia omnis, vitae esse ab, est
+          reprehenderit debitis vel veniam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque similique
+          expedita ipsa. Laudantium vero similique voluptate unde, ratione, praesentium possimus non doloremque adipisci
+          dolorum quaerat ab reiciendis animi? Odit, harum? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis expedita quod, ullam, similique dolorem non animi eaque ea nulla nisi a cum neque rem alias eveniet
+          nam! Nihil, qui necessitatibus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+          exercitationem hic. Quam quo blanditiis distinctio magni ipsa ab, cumque aspernatur doloremque impedit
+          mollitia nam fugiat quaerat deserunt culpa consequuntur facere. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Voluptas, itaque saepe. Perspiciatis dolores maiores quibusdam ea magni? Nisi soluta
+          accusamus, quia omnis, vitae esse ab, est reprehenderit debitis vel veniam. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Itaque similique expedita ipsa. Laudantium vero similique voluptate unde,
+          ratione, praesentium possimus non doloremque adipisci dolorum quaerat ab reiciendis animi? Odit, harum? Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis expedita quod, ullam, similique dolorem non
+          animi eaque ea nulla nisi a cum neque rem alias eveniet nam! Nihil, qui necessitatibus!
         </p>
       </div>
     </div>
--- components/Realizations.js
+++ components/Realizations.js
@@ -1,9 +1,29 @@
+import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export default function Realizations() {
+  const settings = {
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div id="realizations" className="realizations">
       <div className="realizations__container">
         <h2 className="heading text-white">Nasze realizacje</h2>
-        <div className="realizations__box paragraphText">BOX</div>
+        <div className="realizations__cardContainer">
+          <Slider {...settings}>
+            <div>sdkjnsfjdnw</div>
+            <div>slfkmlkfmwlkefmlwkmfwlfkm</div>
+          </Slider>
+        </div>
       </div>
     </div>
   );
