a1a4771323b89370e71f71dc255bab6de03ee0a3
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-11-09 20:54:18 +0100

    added social icons

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "../public/images/Logo.png";
+import Facebook from "../public/images/social/facebook.png";
+import Instagram from "../public/images/social/instagram.png";
 
 export default function Nav() {
   return (
@@ -41,8 +43,12 @@ export default function Nav() {
         </li>
       </ul>
       <div className="nav__socialMediaWrapper visibilityDesktop">
-        <div className="nav__socialMediaIcon mb-4"></div>
-        <div className="nav__socialMediaIcon"></div>
+        <div className="nav__socialMediaIcon mb-4">
+          <Image src={Instagram} alt="Instagram" layout="fill" />
+        </div>
+        <div className="nav__socialMediaIcon">
+          <Image src={Facebook} alt="Facebook" layout="fill" />
+        </div>
       </div>
     </nav>
   );
